1c7d1a0dfd6329dbf703bd38fc74bc3ac21069dc
tomyferra <ferra.tomy@gmail.com>
2024-05-31 09:52:32 -0300

    removed file

--- app.py
+++
@@ -1,9 +0,0 @@
-def insertarEnCadena(cadena, caracter):
-    cadenaNueva = ''
-    for posicion in range(len(cadena)):
-        cadenaNueva = cadenaNueva + cadena[posicion]
-        if ((posicion+1)%3) == 0:
-            cadenaNueva = cadenaNueva + caracter
-    return cadenaNueva
-
-print(insertarEnCadena('2552552550','.'))
